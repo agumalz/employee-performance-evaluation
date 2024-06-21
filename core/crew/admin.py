ed46b7f95549210f177ba2ab8b2b7cd13752179d
BubbleeGum <alzikhriagum@gmail.com>
2024-06-21 21:02:33 +0700

    migrate crew and job model

--- core/crew/admin.py
+++ core/crew/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import Crew, Job
 
 # Register your models here.
+admin.site.register(Crew)
+admin.site.register(Job)
--- core/crew/models.py
+++ core/crew/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+    job_name = models.CharField(max_length=255, unique=True)
+    job_description = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.job_name
+
+class Crew(models.Model):
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    crew_name = models.CharField(max_length=255, unique=True)
+    
+    def __str__(self):
+        return self.crew_name
+    
+    
